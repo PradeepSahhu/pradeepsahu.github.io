c51ff2b2c06d7d0dfa4d11d29d6523c98448274e
Pradeep Sahu <specialreaderman.com@gmail.com>
2022-03-07 15:52:18 +0530

    Website

---
+++ JS/app.js
@@ -0,0 +1,14 @@
+
+
+
+// -------------------------------------------Important------------------------------------------------------------------
+document.querySelector(".education").addEventListener("click", function(){
+  alert("This is a confidential Area. All Documents are private and unauthorized usage of these documents are prohibited ©2022")
+});
+
+document.querySelector(".education").addEventListener("click", function(){
+  document.querySelector(".education .active").style.color = "blue";
+});
+
+
+
